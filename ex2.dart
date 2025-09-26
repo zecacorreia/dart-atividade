0ae41a5de69937e845071793fede9a9dd6c6e304
Italo (Zeca) Correia <128881501+zecacorreia@users.noreply.github.com>
2025-09-26 05:54:13 -0300

    Delete ex2.dart

--- ex2.dart
+++
@@ -1,7 +0,0 @@
-// Escreva um programa em Dart que leia um número e imprima se ele é par ou ímpar.
-String parOuImpar(int n) => n % 2 == 0 ? 'Par' : 'Ímpar';
-
-void main() {
-    int n = 7;
-    print(parOuImpar(n));
-}
